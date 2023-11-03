c8d7cf56895541d77b5f5bd571790641b233535f
GAMING CHAUHAN <143026011+Gamingbuddyyy@users.noreply.github.com>
2023-11-03 23:52:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\nOPSHIVAM 𝐒𝐩𝐚𝐦 𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 😎🤘🏻\nMy Master ---> @itsz_shivam")
+print("\nSAHENSHAH 𝐒𝐩𝐚𝐦 𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 😎🤘🏻\nMy Master ---> @sahenshah01")
 
 
 async def main():
